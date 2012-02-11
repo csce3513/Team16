27bf6db5671299f8e2ab4aee08e2e5e1ac49d170
Jeremy Hornberger <lugies001@gmail.com>
2012-02-11 17:08:56 -0600

    Get background loaded with player moving around

---
+++ dev/resources/bundle.js
@@ -0,0 +1,22 @@
+{
+	// Graphic resources.
+	addImage:[
+		["sprites","resources/cels.png"],	
+		["player_sprite","resources/player_sprite.png"],
+		["font","resources/font.png"],		
+		["logo","resources/logo.png"]
+	],
+	addFont:[
+		{id:"small",image:"font",firstletter:" ",tileh:8,tilew:8,tilerow:255,gapx:0,gapy:0}
+	],
+	addTiles:[
+		{id:"player_tiles",image:"player_sprite",tileh:40,tilew:20,tilerow:1,gapx:0,gapy:0},
+		{id:"tiles",image:"sprites",tileh:20,tilew:20,tilerow:9,gapx:0,gapy:40},
+		{id:"enemy-goo",image:"sprites",tileh:20,tilew:20,tilerow:9,gapx:0,gapy:60},
+		{id:"enemy-sad",image:"sprites",tileh:40,tilew:20,tilerow:9,gapx:0,gapy:80}
+	]//,
+	//addBundle:[
+	//	{file:"resources/bundle-stage1.js"}
+	//]
+	
+}
