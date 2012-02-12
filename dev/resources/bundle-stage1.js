d5758ebf1ba5ac86b099d88aa2c434939d16b74a
Jeremy Hornberger <lugies001@gmail.com>
2012-02-11 23:31:12 -0600

    Added physics and external loading functionality

--- dev/resources/bundle-stage1.js
+++ dev/resources/bundle-stage1.js
@@ -4,17 +4,14 @@
 		{
 			object:"mapmeta",
 			property:"stage1",
-			value:{nextLevel:"stage2"}
+			value:{}
 		},
 		{
 			object:"mapobjects",
 			property:"stage1",
 			value:{
 				items:[
-					{objecttype:"player", x:40, y:180, side:1},
-					{objecttype:"squid", x:80, y:40, side:1},
-					{objecttype:"squid", x:140, y:40, side:1},
-					{objecttype:"squid", x:220, y:40, side:1}
+					{objecttype:"player", x:40, y:180, side:1}
 				]
 			}
 		},
--- dev/resources/bundle.js
+++ dev/resources/bundle.js
@@ -10,13 +10,12 @@
 		{id:"small",image:"font",firstletter:" ",tileh:8,tilew:8,tilerow:255,gapx:0,gapy:0}
 	],
 	addTiles:[
-		{id:"player_tiles",image:"player_sprite",tileh:35,tilew:20,tilerow:3,gapx:0,gapy:0},
+		{id:"player",image:"player_sprite",tileh:35,tilew:20,tilerow:3,gapx:0,gapy:0},
 		{id:"tiles",image:"sprites",tileh:20,tilew:20,tilerow:9,gapx:0,gapy:40},
 		{id:"enemy-goo",image:"sprites",tileh:20,tilew:20,tilerow:9,gapx:0,gapy:60},
 		{id:"enemy-sad",image:"sprites",tileh:40,tilew:20,tilerow:9,gapx:0,gapy:80}
-	]//,
-	//addBundle:[
-	//	{file:"resources/bundle-stage1.js"}
-	//]
-	
+	],
+	addBundle:[
+		{file:"resources/bundle-stage1.js"}
+	]
 }
