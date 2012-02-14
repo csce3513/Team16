0466b4126b7ff98d525c5eccd55f2ff711def4af
Jeremy Hornberger <lugies001@gmail.com>
2012-02-13 22:24:51 -0600

    Added functionality to load enemies

--- dev/resources/bundle-stage1.js
+++ dev/resources/bundle-stage1.js
@@ -11,7 +11,10 @@
 			property:"stage1",
 			value:{
 				items:[
-					{objecttype:"player", x:40, y:180, side:1}
+					{objecttype:"player", x:40, y:180, side:1},
+					{objecttype:"squid", x:80, y:40, side:1},
+					{objecttype:"squid", x:180, y:20, side:1},
+					{objecttype:"squid", x:380, y:20, side:1}
 				]
 			}
 		},
@@ -21,18 +24,18 @@
 			value:help.finalizeTilemap({
 				tileset:"tiles",
 				map:[
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
-					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
-					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
-					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
-					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
-					[0,null,null,null,null,null,null,0,0,null,null,null,null,null,null,0],
-					[0,null,null,0,0,0,null,null,null,null,0,0,0,null,null,0],
-					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
-					[0,0,null,null,null,null,null,null,null,null,null,null,null,null,0,0],
-					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
+					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
+					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
+					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
+					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
+					[0,null,null,null,null,null,null,0,0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
+					[0,null,null,null,null,null,null,null,null,null,0,0,0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
+					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
+					[0,0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0,0],
+					[0,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 				]
 			})
 		}
