8b16c48549cecdb8caf83c97c47fcfca65532123
Araceli Yanez <axy003@uark.edu>
2012-03-01 11:54:38 -0600

    added functionality for boss frames

--- resources/bundle.js
+++ resources/bundle.js
@@ -6,7 +6,8 @@
 		["player_sprite","resources/player_sprite.png"],
 		["font","resources/font.png"],		
 		["logo","resources/logo.png"],
-		["enemysprites","resources/enemysprites.png"]
+		["enemysprites","resources/enemysprites.png"],
+		["tiger_boss","resources/tiger_boss.png"]
 	],
 	addFont:[
 		{id:"small",image:"font",firstletter:" ",tileh:8,tilew:8,tilerow:255,gapx:0,gapy:0}
@@ -15,6 +16,7 @@
 		{id:"player",image:"player_sprite",tileh:35,tilew:20,tilerow:3,gapx:0,gapy:0},
 		{id:"tiles",image:"sprites",tileh:20,tilew:20,tilerow:9,gapx:0,gapy:40},
 		{id:"lsu-tiger",image:"enemysprites",tileh:20,tilew:20,tilerow:3,gapx:0,gapy:0},
+		{id:"tiger-boss",image:"tiger_boss",tileh:50,tilew:30,tilerow:3,gapx:0,gapy:0},
 		{id:"enemy-goo",image:"sprites",tileh:20,tilew:20,tilerow:9,gapx:0,gapy:60},
 		{id:"enemy-sad",image:"sprites",tileh:40,tilew:20,tilerow:9,gapx:0,gapy:80}
 	],
