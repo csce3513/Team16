62202e49fb8b44de4d7ccce3410b7771204de45f
Araceli Yanez <axy003@uark.edu>
2012-03-02 20:59:25 -0600

    added tiger boss resource

--- resources/bundle.js
+++ resources/bundle.js
@@ -5,6 +5,7 @@
 		["bg","resources/bg1.png"],	
 		["font","resources/font.png"],		
 		["logo","resources/logo.png"],
+		["tiger-boss", "resources/tiger_boss.png"],
 		["enemysprites","resources/enemysprites.png"]
 	],	
 	addAudio:[
@@ -18,6 +19,7 @@
 		{id:"player",image:"sprites",tileh:35,tilew:20,tilerow:3,gapx:0,gapy:40},
 		{id:"tiles",image:"sprites",tileh:20,tilew:20,tilerow:9,gapx:0,gapy:0},
 		{id:"lsu-tiger",image:"sprites",tileh:20,tilew:20,tilerow:3,gapx:0,gapy:20},
+		{id:"tiger-boss",image:"tiger-boss",tileh:55,tilew:30,tilerow:3,gapx:0,gapy:0},
 		{id:"crimson-tide",image:"sprites",tileh:25,tilew:40,tilerow:3,gapx:0,gapy:75}
 	],
 	addBundle:[
