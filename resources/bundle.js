22b732852991a0544407fac68ffa8edb79915804
Kevin Gunn <kcgunn@uark.edu>
2012-04-08 22:58:59 -0500

    Implemented HUD for scoring and lives, also refactored code for player.kill

--- resources/bundle.js
+++ resources/bundle.js
@@ -4,6 +4,7 @@
 		["sprites","resources/sprites.png"],
 		["bg","resources/bg1.png"],	
 		["font","resources/font.png"],		
+		["fontbig","resources/fontbig.png"],
 		["logo","resources/logo.png"],
 		["tiger-boss", "resources/tiger_boss.png"],
 		["enemysprites","resources/enemysprites.png"]
@@ -16,14 +17,16 @@
 		["hit-sound",["resources/audio/hit.mp3","resources/audio/hit.ogg"],{channel:"sfx"}]
 	],
 	addFont:[
-		{id:"small",image:"font",firstletter:" ",tileh:8,tilew:8,tilerow:255,gapx:0,gapy:0}
+		{id:"small",image:"font",firstletter:" ",tileh:8,tilew:8,tilerow:255,gapx:0,gapy:0},
+		{id:"big",image:"fontbig",firstletter:" ",tileh:16,tilew:8,tilerow:255,gapx:0,gapy:0}
 	],
 	addTiles:[
 		{id:"player",image:"sprites",tileh:35,tilew:20,tilerow:3,gapx:0,gapy:40},
 		{id:"tiles",image:"sprites",tileh:20,tilew:20,tilerow:9,gapx:0,gapy:0},
 		{id:"lsu-tiger",image:"sprites",tileh:20,tilew:20,tilerow:3,gapx:0,gapy:20},
 		{id:"tiger-boss",image:"tiger-boss",tileh:55,tilew:30,tilerow:3,gapx:0,gapy:0},
-		{id:"crimson-tide",image:"sprites",tileh:25,tilew:40,tilerow:3,gapx:0,gapy:75}
+		{id:"crimson-tide",image:"sprites",tileh:25,tilew:40,tilerow:3,gapx:0,gapy:75},
+		{id:"tiledfont",image:"font",tileh:8,tilew:8,tilerow:255,gapx:0,gapy:8}
 	],
 	addBundle:[
 		{file:"resources/bundle-stage1.js"},
