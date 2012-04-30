afe89e5fb00b9bc3c242227558ab58504d2733a9
David Medina <dmedinar@uark.edu>
2012-04-30 01:01:48 -0500

    Fixes number of enemies in second level

--- resources/bundle-stage2.js
+++ resources/bundle-stage2.js
@@ -12,7 +12,13 @@
 			value:{
 				items:[
 					{objecttype:"player", x:0, y:594, side:1},
-					{objecttype:"crimson-tide", x:180, y:200}
+					
+					{objecttype:"crimson-tide", x:220, y:534},
+					{objecttype:"crimson-tide", x:60, y:400},
+					{objecttype:"crimson-tide", x:200, y:400},
+					{objecttype:"crimson-tide", x:260, y:300},
+					{objecttype:"crimson-tide", x:280, y:200},
+					{objecttype:"crimson-tide", x:180, y:200}	
 				]
 			}
 		},
