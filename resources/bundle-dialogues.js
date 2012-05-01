443bec984adb5056e6ba919f16e114da68a2c9df
Araceli Yanez <axy003@uark.edu>
2012-05-01 00:59:11 -0500

    fixed end credits

--- resources/bundle-dialogues.js
+++ resources/bundle-dialogues.js
@@ -78,93 +78,23 @@
 		  		},
 		  		scenes:[
 		  			{
-		  				slide:{
-		  					image:"ending1",
-		  					x:0,
-		  					y:40
-		  				},
-		  				speed:1,
-		  				who:"narrator",
-		  				audio:"beep",
-		  				talk:[
-		  					"\"Well done!\" Jin said. \"Now I can",
-		  					"go back to my thoughts!\" And he left",
-		  					"behind a big pile of sad violet",
-		  					"squids."
-		  				]
-		  			},
-		  			{
-		  				slide:{
-		  					image:"intro1",
-		  					x:0,
-		  					y:40
-		  				},
-		  				speed:1,
-		  				who:"narrator",
-		  				audio:"beep",
-		  				talk:[
-		  					"\"My life is very very sad\" said.",
-		  					"\"And I feel really lonely here\"."
-		  				]
-		  			},
-		  			{
-		  				slide:{
-		  					image:"intro1",
-		  					x:0,
-		  					y:40
-		  				},
-		  				speed:2,
-		  				who:"narrator",
-		  				audio:"beep",
-		  				talk:[
-		  					"\"I will ever have a friend?\""
-		  				]
-		  			},
-		  			{
 		  				font:"small",
 		  				speed:2,
 		  				spacing:2,
 		  				push:gbox.getScreenHH(),
 		  				audiomusic:"ending",
 		  				scroller:[
-		  					"LEAVE ME ALONE",
-		  					"A SHORT GAME BY KESIEV",
-		  					"","","",
-		  					"JS-CODE BY",
-		  					"KESIEV",
+		  					"Arkansas Rumble",
+		  					"An Awesome Game",
 		  					"","",
-		  					"GRAPHICS BY",
-		  					"KESIEV",
+		  					"BY",
+		  					"Team 16",
 		  					"","",
-		  					"STORY BY",
-		  					"KESIEV",
-		  					"","",
-		  					"SOUND EFFECTS",
-		  					"CREATED USING",
-		  					"CFXR",
-		  					"THIRDCOG.EU/APPS/CFXR",
-		  					"","",
-		  					"MUSIC",
-							"",
-		  					"NIGHT FALLS ON THE BOARDWALK",
-		  					"BY GREENLEO (c)2010",
-		  					"GREENLEO.BANDCAMP.COM",
-		  					"",
-		  					"FOAMY PIXEL WAVE",
-		  					"BY GREENLEO (c)2010",
-		  					"GREENLEO.BANDCAMP.COM",
-		  					"",
-		  					"A DREAM OF THE SEA",
-		  					"BY SMILETRON (c)2009",
-		  					"8BITCOLLECTIVE.COM/MEMBERS/SMILETRON",
-		  					"","","",		  					
-		  					"CREATED USING THE AKIHABARA ENGINE",
-		  					"BY KESIEV (c)2010",
-		  					"WWW.KESIEV.COM",
-							"","","","","","","","","","","",
-							"THANK YOU FOR PLAYING!",
-							"NEVER FORGET WHO LOVES YOU.",
-							"",
+		  					"Jeremy Hornberger",
+		  					"David Medina",
+		  					"Kevin Gunn",
+		  					"Araceli Yanez",
+							"","",
 							"THE END"
 
 		  				]
